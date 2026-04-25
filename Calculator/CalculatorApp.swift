e415ad456bc927d51ce6d674d86c5fc20ef5f7ab
had0Uken! <57702914+DedushkaPlay@users.noreply.github.com>
2026-04-25 11:53:49 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Lev on 25.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
